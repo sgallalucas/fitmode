bab8c51f2bb8b30aad16ad92530ce239aa609638
Lucas Sgalla <lucasgalla10@gmail.com>
2025-08-13 20:21:36 -0300

    Projeto criado

---
+++ src/main/java/com/sgallalucas/FitMode/FitModeApplication.java
@@ -0,0 +1,13 @@
+package com.sgallalucas.FitMode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitModeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitModeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgallalucas/FitMode/FitModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgallalucas.FitMode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitModeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
